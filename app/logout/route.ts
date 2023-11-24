260e4517e6007fbdb0b4717dd9c13a623f4fcdbe
Jason Lengstorf <jason@lengstorf.com>
2023-11-23 20:58:02 -0800

    fix: remove duplicate redirect

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -1,12 +1,12 @@
-import { signOut } from '@/app/auth';
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export function GET(request: Request) {
 	const url = new URL(request.url);
 	url.pathname = '/';
 	const response = NextResponse.redirect(url);
 
-	signOut();
+	cookies().delete('token');
 
 	return response;
 }
